758e33a7bdf04e1b9779137a98903532471b69ef
songliang.cai <1298587604@qq.com>
2024-08-22 10:50:21 +0800

    移除Component

--- app/src/main/java/com/example/jitpackmvn/TestActivity.kt
+++ app/src/main/java/com/example/jitpackmvn/TestActivity.kt
@@ -13,7 +13,7 @@ class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.activity_test)
     }
 
 }
